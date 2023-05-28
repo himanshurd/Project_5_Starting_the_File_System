37e97f7c6af4aeafb44404c0d3f3a5d4d30b1291
himanshurd <91275023+himanshurd@users.noreply.github.com>
2023-05-28 12:10:43 -0700

    add functionality to mkfs

--- mkfs.c
+++ mkfs.c
@@ -1,6 +1,13 @@
 #include "mkfs.h"
 #include "block.h"
 #include "image.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "inode.h"
+#include "pack.h"
 
 void mkfs(void){
     for(int i=0; i < NUMBER_OF_BLOCKS; i++){
@@ -9,5 +16,16 @@ void mkfs(void){
     for(int i=0; i < 7; i++){
         alloc();
     }
-    return;
+    struct inode *root_node = ialloc();
+    int root_block = alloc();
+    root_node->block_ptr[0] = root_block;
+    root_node->flags = 2;
+    root_node->size = 32*2;
+    unsigned char block[BLOCK_SIZE];
+    write_u16(block, root_node->inode_num);
+    strcpy((char*)block+2, ".");
+    write_u16(block+32, root_node->inode_num);
+    strcpy((char*)block+2+32, "..");   
+    bwrite(root_node->inode_num, block);
+    iput(root_node);
 }
