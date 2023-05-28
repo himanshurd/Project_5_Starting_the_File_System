b70f7942ed62a36d7bf5a8391a0907679d06c9b9
himanshurd <91275023+himanshurd@users.noreply.github.com>
2023-05-28 13:16:44 -0700

    complete implementation in mkfs

--- mkfs.c
+++ mkfs.c
@@ -29,3 +29,39 @@ void mkfs(void){
     bwrite(root_node->inode_num, block);
     iput(root_node);
 }
+
+struct directory *directory_open(int inode_num) {
+    struct inode *directory_node = iget(inode_num);
+    if (directory_node == NULL) {
+        return NULL;
+    }
+    struct directory *dir = malloc(sizeof(struct directory));
+    dir->inode = directory_node;
+    dir->offset = 0;
+    return dir;
+}
+
+int directory_get(struct directory *dir, struct directory_entry *ent)
+{
+    if (dir->offset >= dir->inode->size) {
+        return -1;
+    }
+
+    int data_block_index = dir->offset / 4096;
+    int data_block_num = dir->inode->block_ptr[data_block_index];
+
+    unsigned char block[BLOCK_SIZE];
+    bread(data_block_num, block);
+
+    int offset_in_block = dir->offset % BLOCK_SIZE;
+    ent->inode_num = read_u16(block + offset_in_block);
+    strcpy(ent->name, (char *)block + 2);
+    dir->offset += 32;
+
+    return 0;
+}
+
+void directory_close(struct directory *d) {
+    iput(d->inode);
+    free(d);
+}
